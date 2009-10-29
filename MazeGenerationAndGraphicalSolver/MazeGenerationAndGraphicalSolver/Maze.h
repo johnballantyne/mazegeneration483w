01311b1597d85b65bc03b3410cf4a6165b9d8c66
johnballantyne <johnballantyne@f1cf40a4-bd93-11de-8784-f7ad4773dbab>
2009-10-29 15:53:31 +0000

    Added Maze Class

--- MazeGenerationAndGraphicalSolver/MazeGenerationAndGraphicalSolver/Maze.h
+++ MazeGenerationAndGraphicalSolver/MazeGenerationAndGraphicalSolver/Maze.h
@@ -1,54 +1,27 @@
-//#pragma once
-#include <iomanip>
+#ifndef CUBE_H
+#define CUBE_H
 #include <vector>
-#include "Coordinate.h"
 using namespace std;
 
 class Maze
 {
 public:
-	Maze()
-	{
-		Width = Length = 0;
-		//myVector(Width, vector<bool>(Length));
-		for ( int i = 0; i < Width; i++ )
-		{
-			//vector<bool> temp;
-			//temp.resize(10);
-			for (int j = 0; j < Length; j++)
-			{
-				myVector[i].push_back(1);
-			}
-		}
-	};
-	//init constructor
-	Maze(int newWidth, int newLength)
-	{
-		SetWidth(newWidth);
-		SetLength(newLength);
-		//myVector(Width, vector<bool>(Length));
-		for ( int i = 0; i < Width; i++ )
-		{
-			for (int j = 0; j < Length; j++)
-			{
-				myVector[i].push_back(1);
-			}
-		}
-	};
-	bool CheckCoordinates(Coordinates myCoordinate)
-	{
-		return myVector[myCoordinate.x][myCoordinate.y];
-	};
- 	void SetWidth(int newWidth)
-	{
-		Width = newWidth;
-	}
-	void SetLength(int newLength)
-	{
-		Length = newLength;
-	}
+	Maze();
+	Maze(int w, int l);
+	void setWidth(int w);
+	void setLength(int l);
+	int getWidth();
+	int getLength();
+	bool checkCoordinate(int x, int y);
+	void setCoordinate(int x, int y, bool set);
+	void printMaze();
+	void GenMaze_Recursive();
 private:
-
-	vector<vector<bool> > myVector;
-	int Width, Length;
+	void GenMaze_RecursiveFunction(int x1, int x2, int y1, int y2);
+	int width;
+	int length;
+	int real_width;
+	int real_length;
+	vector<vector<bool>> maze;
 };
+#endif
