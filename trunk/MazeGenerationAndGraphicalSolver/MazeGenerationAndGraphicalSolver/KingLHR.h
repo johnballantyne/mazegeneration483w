9d209ccffe3e515353f399a69b7e2c2446682364
bnk5001 <bnk5001@f1cf40a4-bd93-11de-8784-f7ad4773dbab>
2009-11-12 15:48:42 +0000

    version from home

---
+++ trunk/MazeGenerationAndGraphicalSolver/MazeGenerationAndGraphicalSolver/KingLHR.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "King.h"
+
+class KingLHR : public King
+{
+	void Solve(Coordinates Start, Coordinates End, Direction startDirection)
+	{
+		do{
+			while(GetDirection() == North){
+				if(!LookNorth())
+					GetLocation().MoveZ(-1);
+				else SetDirection(West);
+			}
+			while(GetDirection() == West){
+				if(!LookWest())
+					GetLocation().MoveX(-1);
+				else SetDirection(South);
+			}
+			while(GetDirection() == South){
+				if(!LookSouth())
+					GetLocation().MoveZ(1);
+				else SetDirection(East);
+			}
+			while(GetDirection() == East){
+				if(!LookEast())
+					GetLocation().MoveX(1);
+				else SetDirection(North);
+			}
+		}
+		while(GetLocation().GetCoordinates() != End);
+	}
+};
