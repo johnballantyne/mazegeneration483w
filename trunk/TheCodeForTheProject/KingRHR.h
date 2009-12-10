6e11b9355fa7be1d2dda21eb5339ca6b95888204
bnk5001 <bnk5001@f1cf40a4-bd93-11de-8784-f7ad4773dbab>
2009-12-10 00:25:49 +0000

    final revisions

--- trunk/TheCodeForTheProject/KingRHR.h
+++ trunk/TheCodeForTheProject/KingRHR.h
@@ -8,36 +8,43 @@ public:
 	{
 		SetLocation(Start);
 		PushPath(GetLocation());
-
 		do{
 			while(GetDirection() == North){
+				if(GetLocation().GetCoordinates() == End) break;
 				if(!LookNorth() && LookWest()){
 					this->Move(North);
 					PushPath(GetLocation());
+					this->SetMazeCoordinate(GetLocation(), true);
 				}
 				else if(!LookWest()) SetDirection(West);
 				else SetDirection(East);
 			}
 			while(GetDirection() == East){
+				if(GetLocation().GetCoordinates() == End) break;
 				if(!LookEast() && LookNorth()){
 					this->Move(East);
 					PushPath(GetLocation());
+					this->SetMazeCoordinate(GetLocation(), true);
 				}
 				else if(!LookNorth()) SetDirection(North);
 				else SetDirection(South);
 			}
 			while(GetDirection() == South){
+				if(GetLocation().GetCoordinates() == End) break;
 				if(!LookSouth() && LookEast()){
 					this->Move(South);
 					PushPath(GetLocation());
+					this->SetMazeCoordinate(GetLocation(), true);
 				}
 				else if(!LookEast()) SetDirection(East);
 				else SetDirection(West);
 			}
 			while(GetDirection() == West){
+				if(GetLocation().GetCoordinates() == End) break;
 				if(!LookWest() && LookSouth()){
 					this->Move(West);
 					PushPath(GetLocation());
+					this->SetMazeCoordinate(GetLocation(), true);
 				}
 				else if (!LookSouth()) SetDirection(South);
 				else SetDirection(North);
@@ -45,4 +52,5 @@ public:
 		}
 		while(GetLocation().GetCoordinates() != End);
 	}
+
 };
